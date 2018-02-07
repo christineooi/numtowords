14fedeb9a27c0338e193f15ff1b74bc364935a69
Christine Ooi <christine.ooi@gmail.com>
2018-02-07 15:33:24 -0500

    modified js file

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ function getOnes(num){
     return ones[num-1];
 }
 
+function getTwoDigit(num){
+    var tdStr = num.toString();
+    var firstdigit = tdStr.substr(0,1);
+    var seconddigit = tdStr.substr(1,1);
+    var newStr = "";
+    if (seconddigit === "0"){     
+        newStr = tens[firstdigit-1];
+    } else if (firstdigit === "1"){ 
+        newStr = teens[seconddigit-1];
+    } else { 
+        newStr = tens[firstdigit-1] + " " + getOnes(seconddigit); 
+    }
+    return newStr;
+}
+
 function numToWords(num){
     var wordStr = "";
     numStr = num.toString().trim();
@@ -15,24 +30,26 @@ function numToWords(num){
     if (strLen === 1){
         wordStr = getOnes(num);
     } else if (strLen === 2){
+        wordStr = getTwoDigit(num);
+    } else if(strLen == 3) {
         var firstdigit = numStr.substr(0,1);
-        var seconddigit = numStr.substr(1);
-        console.log("first digit: " + firstdigit);
-        console.log("second digit: " + seconddigit);
-        if (seconddigit === "0"){
-            wordStr = tens[firstdigit-1];
-        } else if (firstdigit === "1"){
-            wordStr = teens[seconddigit-1];
+        var seconddigit = numStr.substr(1,1);
+        var thirddigit = numStr.substr(2,1); 
+        var twodigit = numStr.substr(1,2);
+        if (seconddigit === "0" && thirddigit === "0"){
+            wordStr = ones[firstdigit-1] + " hundred";
+        } else if (seconddigit === "0"){
+            wordStr = ones[firstdigit-1] + " hundred " + getOnes(thirddigit);
         } else {
-            wordStr = tens[firstdigit-1] + " " + getOnes(seconddigit); 
+            wordStr = ones[firstdigit-1] + " hundred " + getTwoDigit(twodigit);
         }
+
     }
-    // console.log("Word: " + wordStr);
-    // document.getElementById("results").innerHTML = wordStr + "<br>";
+
     return wordStr;
 }
 
-for (var i = 1; i < 100; i++){
+for (var i = 1; i < 1000; i++){
     
     resultStr += i + ": " + numToWords(i) + "<br>";
 }
